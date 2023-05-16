bf62bc7e09fb2d39b3aba5dbcd8d22a9ea57e52a
User <sdg0627@naver.com>
2023-05-16 14:26:59 +0900

    연습

--- src/components/NavCom.vue
+++ src/components/NavCom.vue
@@ -1,15 +1,61 @@
 <template>
-  <div>
-    네비게이션
+  skjdhflksdjf;ljsd;lfljsd
+  <div class="w-full">
+      <div class="max-w-7xl mx-auto justify-between flex">
+          
+        <!-- logo -->
+        <div class="basis-1/4">
+             <img src="http://via.placeholder.com/120x50" alt="">
+        </div>
+
+        <!-- navi bar -->
+        <div class="basis-2/4 border">
+            <ul class="basis-2/4 flex justify-between gap-x-[30px]">
+              <li  v-for="(e,index) in NavList[0]" :key="e" >
+                      <router-link :to="NavList[1][index]">{{ e }} </router-link></li>
+            </ul>
+        </div>
+
+        <!-- login bar -->
+        <!-- <ul class="ml-10 basis-1/4 flex justify-between gap-x-12">
+          <li>로그인</li>          
+          <p>로그아웃</p>          
+          <p>회원가입</p>          
+        </ul> -->
+        <div class="basis-1/6 ml-5 border">
+            <ul class="flex justify-between gap-x-[5%] ">
+                <li v-if="!$store.state.loginChk"><router-link to="/login">로그인</router-link> </li>
+                <li v-else @click="logout"><router-link to="/">로그아웃</router-link></li>
+                <li>회원가입</li>
+            </ul>
+
+        </div>
+
+      </div>
   </div>
 </template>
 
 <script>
+import NavData from '../assets/Data.json'
+import {auth} from '../firebase'
   export default {
-    
+    name:"NavCom",
+    data() {
+      return {
+        NavList:NavData.Nav
+
+      }
+    },
+    methods :{
+      logout(){
+        auth.signOut();
+        this.$store.commit('logOutToken')
+      }
+    }
   }
 </script>
 
 <style>
+li{ list-style:none; }
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    loginChk:false
   },
   mutations: {
     loginToken(state,payload){
       state.loginToken = payload.refreshToken
       state.loginChk =true
       state.uid=payload.uid
+    },
+    logOutToken(state){
+      state.loginChk = false,
+      state.loginToken= null,
+      state.displayName = "",
+      state.uid = ''
+      sessionStorage.removeItem("refreshToken")
+      sessionStorage.removeItem("displayName")
     }
   },
 })
