00af9e932e695f18753b0617ccbe91e13be67577
User <sdg0627@naver.com>
2023-05-18 15:59:29 +0900

    1559

--- src/components/FooterCom.vue
+++ src/components/FooterCom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full border-red-500 border relative bg-yellow-200">
+  <div class="w-full border-red-500 border relative">
     
       <div class="max-w-7xl flex justify-center gap-x-[5%] mx-auto border-blue-500 border">
           <div class="border">
@@ -9,7 +9,7 @@
             <p class=" text-center text-gray-400">개인정보처리방침</p>
             <p class=" text-center text-gray-400">이메일무단수집</p>
           </div>
-          <div class="mt-10">
+          <div class="mt-16">
               <div class="flex justify-center">
                 <span class="relative after:absolute after:w-0.5 after:bg-black after:h-4 mr-3 after:-right-2 after:bottom-0.5">CompanyName</span>
                 <span>대표자 : 홍길동</span>
