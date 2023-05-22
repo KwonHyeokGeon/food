4a8c7e7379771720d2bd83632957d3c9fd757a15
User <sdg0627@naver.com>
2023-05-22 15:52:03 +0900

    히든 로그 회원 아이콘

--- src/components/NavCom.vue
+++ src/components/NavCom.vue
@@ -17,9 +17,9 @@
     </div>
     <div class="basis-1/6 hidden lg:block ">
       <ul class="flex justify-center gap-x-[2%]">
-        <li class="font-extrabold relative" v-if="!$store.state.loginChk"><router-link to="/login"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock " class="w-3 absolute mt-1 -ml-3"> 로그인</router-link> </li>
-        <li class="font-extrabold relative" v-else @click="logout"><router-link to="/"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock" class="w-3 absolute mt-1 -ml-4">로그아웃</router-link></li>
-        <li class="font-extrabold ml-7 relative" v-if="!$store.state.loginChk"><router-link to="/member"><img :src="require('@/assets/img/user-solid.svg')" alt="user" class="w-3 absolute mt-1 -ml-3">회원가입</router-link></li>
+        <li class="font-extrabold relative" v-if="!$store.state.loginChk"><router-link to="/login"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock " class="w-3 absolute mt-1 -ml-4"> 로그인</router-link> </li>
+        <li class="font-extrabold relative" v-else @click="logout"><router-link to="/"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock" class="w-3 absolute mt-1 -ml-5">로그아웃</router-link></li>
+        <li class="font-extrabold ml-7 relative" v-if="!$store.state.loginChk"><router-link to="/member"><img :src="require('@/assets/img/user-solid.svg')" alt="user" class="w-3 absolute mt-1 -ml-4">회원가입</router-link></li>
       </ul>
     </div>
     </div>
@@ -71,9 +71,9 @@
     <img src="@/assets/img/logo_white.png" alt="" class="w-[120px] mx-auto mt-10">
         <!--  -->
       <ul class="w-[190px] flex justify-center gap-x-[2%] border-b border-gray-400 pb-3 mx-auto mt-10">
-        <li class="font-extrabold" v-if="!$store.state.loginChk"><router-link to="/login">로그인</router-link> </li>
-        <li class="font-extrabold" v-else @click="logout"><router-link to="/">로그아웃</router-link></li>
-        <li class="font-extrabold ml-3" v-if="!$store.state.loginChk"><router-link to="/member">회원가입</router-link></li>
+        <li class="font-extrabold mr-4" v-if="!$store.state.loginChk"><router-link to="/login"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock " class="w-3 absolute mt-1 -ml-4">로그인</router-link> </li>
+        <li class="font-extrabold" v-else @click="logout"><router-link to="/"><img :src="require('@/assets/img/lock-solid.svg')" alt="lock " class="w-3 absolute mt-1 -ml-4">로그아웃</router-link></li>
+        <li class="font-extrabold ml-3" v-if="!$store.state.loginChk"><router-link to="/member"><img :src="require('@/assets/img/user-solid.svg')" alt="user" class="w-3 absolute mt-1 -ml-4">회원가입</router-link></li>
       </ul>
         <!--  -->
 
