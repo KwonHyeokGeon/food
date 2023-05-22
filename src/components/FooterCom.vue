dd38f289e6e5ef4851e69bbee5783f42416acd1d
User <sdg0627@naver.com>
2023-05-22 10:31:48 +0900

    230522-AM-1031

--- src/components/FooterCom.vue
+++ src/components/FooterCom.vue
@@ -1,35 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="w-full border relative bg-yellow-200">
-    
-      <div class="max-w-7xl lg:flex lg:justify-center lg:gap-x-[5%] mx-auto">
-          <div class="border">
-            <img src="../assets/img/logo.png" alt="logo" class="w-48 text-center mx-auto">
-            <p class=" text-center text-gray-400">이용약관</p>
-            <p class=" text-center text-gray-400">개인정보처리방침</p>
-            <p class=" text-center text-gray-400">이메일무단수집</p>
-          </div>
-          <div>
-              <div class="flex justify-center lg:mt-20">
-                <span class="relative after:absolute after:w-0.5 after:bg-black after:h-4 mr-3 after:-right-2 after:bottom-1">CompanyName</span>
-                <span>대표자 : 홍길동</span>
-              </div>
-              <p class="text-center">주소 :  대구광역시 중구 중앙대로 394 제일빌딩 5F</p>
-              <div class="mx-auto flex justify-center flex-wrap">
-                  <p class="mr-2">Tel : 053-572-1005</p>
-                  <p class="mr-2">FAX : 053-572-1005</p>
-                  <p class="mr-2">E-mail : abcd@gmail.com</p>
-              </div>
-              <p class="text-center"> 2023.(저작권자) all rights reserved</p>
-          </div>
-      </div>
-=======
-  <div class="border-t border-b h-[70px] bg-vege-600 flex justify-center text-white/40 cursor-pointer">
-    <p class="mx-4 mt-6">이용약관</p>
-    <p class="mx-4 mt-6">개인정보처리방침</p>
-    <p class="mx-4 mt-6">이메일무단수집거부</p>
->>>>>>> 31dd3efd0c4d9f3cb79638286ad88e8d3f57607f
-  </div>
   <div class="h-[370px] bg-vege-600 flex justify-center text-white relative">
     <!-- left area -->
     <div class="mt-14">
