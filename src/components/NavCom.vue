a6f62d6e7caeb1a3abe3da80ce2665a1a7e6ffe5
User <sdg0627@naver.com>
2023-05-22 09:56:25 +0900

    230522 0956

--- src/components/NavCom.vue
+++ src/components/NavCom.vue
@@ -56,23 +56,15 @@
   <!-- 우측 hidden 메뉴 Click 사용!! 삼항O -->
   <div :class="isOpen && '!right-0'"
     class="w-80 bg-gray-500 h-full z-30 fixed top-0 transiton-all duration-500 -right-80 lg:hidden">
-    <ul class=" mt-20">
-      <li v-for="(e, index) in NavList[0]" :key="e"
-        v-on:click="NavSelectIndex = index; SubDown(index); isSubOpen == false ? isSubOpen = true : isSubOpen = false"
-        class="text-center hover:font-extrabold">{{ e }}
-        <ul :style="isSubOpen && isSubDown" class="submenu text-center h-0 overflow-hidden transition-all duration-500">
-          <li v-for="(el, i) in SubList[index]" :key="el"><router-link :to="SubMenuLink[index][i]"
-              class="hover:text-red-500"> {{ el }}</router-link></li>
-        </ul>
-      </li>
-    </ul>
-  </div>
 
+    <ul class="flex justify-start gap-x-[2%] text-white mt-2 ml-2">
+        <li class="font-extrabold" v-if="!$store.state.loginChk"><router-link to="/login">로그인</router-link> </li>
+        <li class="font-extrabold" v-else @click="logout"><router-link to="/">로그아웃</router-link></li>
+        <li class="font-extrabold ml-3" v-if="!$store.state.loginChk"><router-link to="/member">회원가입</router-link></li>
+      </ul>
 
-
-  <!-- 우측 hidden 메뉴 Click 사용!! 삼항X -->
-  <div :class="isOpen && '!right-0'" class="w-80 bg-gray-500 h-full z-30 fixed top-0 transiton-all duration-500 -right-80 lg:hidden ">
-        <ul class=" mt-20">
+      
+      <ul class=" mt-20">
             <li v-for="(e,index) in NavList[0]" :key="e" v-on:click="NavSelectIndex=index;SubDown(index)" class="text-center hover:font-extrabold"  :class="isSubOpen=false ? isSubOpen=true : false"><router-link :to="NavList[1][index]" class="hover:text-green-500">{{ e }}</router-link>
                  <ul  :style="NavSelectIndex==index && isSubDown" class="submenu text-center h-0 overflow-hidden transition-all duration-500">
                      <li v-for="(el,i) in SubList[index]" :key="el"><router-link :to="SubMenuLink[index][i]" class="hover:text-red-500"> {{ el }}</router-link></li>
@@ -80,6 +72,22 @@
             </li>
         </ul>
   </div>
+
+
+
+  <!-- 우측 hidden 메뉴 Click 사용!! 삼항X -->
+  <!-- <div :class="isOpen && '!right-0'" class="w-80 bg-gray-500 h-full z-30 fixed top-0 transiton-all duration-500 -right-80 lg:hidden ">
+
+       
+
+        <ul class=" mt-20">
+            <li v-for="(e,index) in NavList[0]" :key="e" v-on:click="NavSelectIndex=index;SubDown(index)" class="text-center hover:font-extrabold"  :class="isSubOpen=false ? isSubOpen=true : false"><router-link :to="NavList[1][index]" class="hover:text-green-500">{{ e }}</router-link>
+                 <ul  :style="NavSelectIndex==index && isSubDown" class="submenu text-center h-0 overflow-hidden transition-all duration-500">
+                     <li v-for="(el,i) in SubList[index]" :key="el"><router-link :to="SubMenuLink[index][i]" class="hover:text-red-500"> {{ el }}</router-link></li>
+                 </ul>
+            </li>
+        </ul>
+  </div> -->
 
   <!-- 우측 hidden 메뉴 MouseOver 사용!!
   <div :class="isOpen && '!right-0'" class="w-80 bg-gray-500 h-full z-30 fixed top-0 transiton-all duration-500 -right-80 lg:hidden">
