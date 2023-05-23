8cff997b9ce99d8dec4deffb37cd7f1aa56fddb5
bundasse <ymtotolove@gmail.com>
2023-05-23 18:10:21 +0900

    notice, qna writepage

--- src/views/Cs/QnaWrite.vue
+++ src/views/Cs/QnaWrite.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="flex flex-wrap">
-        <label for="title">제목</label><input v-model="title" type="text" id="title" class="border basis-5/12">
-        <label for="author">작성자</label><input v-model="author" type="text" id="author" class="border basis-5/12">
+    <div class="w-full border-y py-10 border-vege-600">
+        <div class="flex flex-wrap justify-between items-center">
+            <label for="title" class="basis-1/12">제목</label><input v-model="title" type="text" id="title" class="border basis-7/12 px-2 py-1">
+            <label for="author" class="basis-1/12">작성자</label><input v-model="author" type="text" id="author" class="border basis-2/12 px-2 py-1">
+        </div>
+        <textarea v-model="content"  class="border w-full mt-2 p-3"></textarea>
     </div>
-    <textarea v-model="content"  class="border w-full"></textarea>
-    <div>
+    <div class="w-full flex justify-end pt-10">
         <button @click="write" class="px-4 py-2 rounded text-white bg-vege-200 hover:bg-vege-400">등록하기</button>
     </div>
 </template>
