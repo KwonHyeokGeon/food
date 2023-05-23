5fa0ae7d3b4d2aaece169a58d8112f41a66b8d55
User <sdg0627@naver.com>
2023-05-23 17:02:16 +0900

    네비 고정 완료

--- src/components/NavCom.vue
+++ src/components/NavCom.vue
@@ -77,9 +77,9 @@
 
 
   <!-- 우측 hidden 메뉴 Click 사용!! 삼항X -->
-  <div :class="isOpen && '!right-0'" class="w-80 bg-mayo h-full z-40 fixed top-0 transiton-all duration-500 -right-80 md:hidden">
+  <div :class="isOpen && '!right-0'" class="w-80 bg-mayo h-full z-40 top-0 transiton-all duration-500 -right-80 fixed">
     <img src="@/assets/img/logo_white.png" alt="" class="w-[120px] mx-auto mt-10">
-    <p class="mr-7 text-point text-center font-extrabold mt-5">{{ $store.state.displayName }}<span class="pl-2 text-black">님</span></p>
+    <p class="mr-7 text-point text-center font-extrabold mt-5">{{ $store.state.displayName }}<span v-if="$store.state.displayName !==''" class="pl-2 text-black">님</span></p>
         <!--  -->
       <ul class="w-[190px] flex justify-center gap-x-[2%] border-b border-gray-400 pb-3 mx-auto mt-5">
         <li class="font-extrabold mx-auto" v-if="!$store.state.loginChk"><router-link to="/login"><img
